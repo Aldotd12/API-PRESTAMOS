a382528d2f8f1d02d42fde2be28a548c3ae835ba
Aldotd12 <tolentinodomingodiego@gmail.com>
2025-03-25 21:25:02 -0600

    aqui new

--- py-linux/main.py
+++ py-linux/main.py
@@ -25,7 +25,7 @@ app.include_router(loan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://prestamosuniversitarios.vercel.app"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
