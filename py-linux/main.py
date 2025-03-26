1c08f7b0857a003c3b424e7b67b6ddda3ba0d59c
Aldotd12 <tolentinodomingodiego@gmail.com>
2025-03-25 22:40:27 -0600

    regresando al inicio

--- py-linux/main.py
+++ py-linux/main.py
@@ -1,32 +1,33 @@
-import os
-import uvicorn
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from routes.usersRoutes import user, auth_router
+from fastapi import FastAPI, Depends
+from fastapi.security import HTTPBearer
+from routes.usersRoutes import user, auth_router  # Asegúrate de importar auth_router
 from routes.materialRoutes import material
 from routes.loanRoutes import loan
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+origins = [
+    "http://localhost:3000",
+    "https://frontsito-prestamos.vercel.app/"
+]
 
-# Configurar CORS correctamente
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://frontsito-prestamos.vercel.app"],
-    allow_credentials=True,
-    allow_methods=["*"],  # Permitir todos los métodos
-    allow_headers=["*"],  # Permitir todos los headers
+app = FastAPI(
+    title="API  ",
+    description="API de pruebas con rutas protegidas "
 )
 
-# Registrar las rutas después de configurar CORS
+# Configurar autenticación con HTTPBearer
+security = HTTPBearer()
+
+# Registrar rutas (agregar autenticación solo en rutas protegidas)
 app.include_router(user)
-app.include_router(auth_router)
+app.include_router(auth_router)  
 app.include_router(material)
 app.include_router(loan)
 
-@app.get("/")
-async def root():
-    return {"message": "API is running"}
-
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", 10000))  # Render usa el puerto 10000 por defecto
-    uvicorn.run(app, host="0.0.0.0", port=port)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000","https://frontsito-prestamos.vercel.app/"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
